80d32d2e219a4622374883c1ccf5e3be17592190
sofiathefirst <843024750@qq.com>
2017-05-10 11:13:43 +0800

    show chinese

---
+++ plot_chinese.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+import matplotlib  
+matplotlib.use('qt4agg')  
+from matplotlib.font_manager import *  
+import matplotlib.pyplot as plt  
+#定义自定义字体，文件名从1.b查看系统中文字体中来  
+myfont = FontProperties(fname='/usr/share/fonts/truetype/wqy/wqy-microhei.ttc')  
+#解决负号'-'显示为方块的问题  
+matplotlib.rcParams['axes.unicode_minus']=False  
+plt.plot([-1,2,-5,3])  
+plt.title(u'中文',fontproperties=myfont)  
+plt.show()
